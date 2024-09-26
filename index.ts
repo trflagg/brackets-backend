a725c4c47f40849cb3db4803b04a0c0e92185bce
Taylor Flagg <trflagg@gmail.com>
2024-09-25 22:12:03 -0500

    Add GET /bracket/:id handler

--- index.ts
+++ index.ts
@@ -1,11 +1,30 @@
 import express from "express";
 import { db } from "./src/db";
 import { bracket } from "./src/schema";
+import { eq } from "drizzle-orm";
 const app = express();
 const port = 3000;
 
 app.get("/brackets", async (req, res) => {
   const result = await db.select().from(bracket);
+  res.json({ result });
+});
+
+app.get("/bracket/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res.send(400);
+  }
+  if (isNaN(+id)) {
+    return res.send(400);
+  }
+  const result = await db
+    .select({
+      id: bracket.id,
+      name: bracket.name,
+    })
+    .from(bracket)
+    .where(eq(bracket.id, new Number(id) as number));
   res.json({ result });
 });
 
